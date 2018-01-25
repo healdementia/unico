b4dcf4363eb6ef9b3feb020ca1ccdc2f658675aa
Marina Kuznetsova <infans.autumnale@gmail.com>
2018-01-25 12:04:49 +0400

    Add the index page markup and pre-processor files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var autoprefixer = require("autoprefixer");
+var postcss = require("gulp-postcss");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("img/sprite-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
