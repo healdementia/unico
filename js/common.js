c772ce29f2f52ed2f1e2f69a404b721ea4701572
Marina Kuznetsova <infans.autumnale@gmail.com>
2018-01-28 20:40:48 +0400

    Finish the index page - add popup, sticky menu and some JS to make this all work

--- js/common.js
+++ js/common.js
@@ -1,3 +1,29 @@
 $('.slider').slick({
   dots: true
 });
+
+document.getElementById('calculate-button').onclick = function() {
+  document.getElementById('overlay').classList.add('display-block');
+  document.getElementById('overlay-holder').classList.add('display-flex');
+}
+
+document.getElementById('calculate-close').onclick = function() {
+  document.getElementById('overlay').classList.remove('display-block');
+  document.getElementById('overlay-holder').classList.remove('display-flex');
+};
+
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled >= 160) {
+    document.getElementById('main-nav').classList.add('main-nav--sticky');
+    document.getElementById('back-wrapper').classList.add('back-wrapper--sticky')
+  } else {
+    document.getElementById('main-nav').classList.remove('main-nav--sticky');
+    document.getElementById('back-wrapper').classList.remove('back-wrapper--sticky')
+  }
+};
+
+document.getElementById('menu-open').onclick = function() {
+  document.getElementById('main-nav').classList.toggle('display-flex');
+  document.getElementById('menu-open').classList.toggle('top__open-menu--close')
+};
